fb2c43be592b2edad5fa084a6bdc76fad97c5895
rymi77 <mil16035@byui.edu>
2021-01-26 10:07:49 -0700

    transfered build to src

---
+++ src/js/productData.js
@@ -0,0 +1,20 @@
+class ProductData{
+    constructor();
+    convertToJson(t) {
+        if (t.ok) return t.json();
+        throw new Error("Bad Response");
+    }
+    
+    getProductsData() {
+        fetch("../json/tents.json")
+          .then(convertToJson)
+          .then((t) => {
+            products = t;
+        });
+    }
+
+    findProductById(t){
+        const e = products.find((n) => n.Id === t.target.dataset.id);
+    }
+}
+export default ProductData;
