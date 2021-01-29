c915e19b3171c7e9e6c701e27ca5ec1b6ce9bce5
rymi77 <mil16035@byui.edu>
2021-01-29 12:38:53 -0700

    changes stuff

--- src/js/utils.js
+++ src/js/utils.js
@@ -24,3 +24,11 @@ export function getParams(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+
+export function renderListWithTemplate(template, parent, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
