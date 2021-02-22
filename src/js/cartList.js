5b495c5992d026571c7c4882fe0284a712980066
rymi77 <mil16035@byui.edu>
2021-02-22 15:58:03 -0700

    added some stuff

--- src/js/cartList.js
+++ src/js/cartList.js
@@ -1,4 +1,4 @@
-import { renderListWithTemplate, getLocalStorage } from './utils.js';
+import { renderListWithTemplate, getLocalStorage, setLocalStorage } from './utils.js';
 
 export default class CartList {
   constructor (key, listElement) {
@@ -10,8 +10,23 @@ export default class CartList {
     
     const list = getLocalStorage(this.key);
     this.renderList(list);
+    this.listElement.addEventListener('click', this.removeFromCart.bind(this));
   }
   
+  removeFromCart(e) {
+    let cartContents = getLocalStorage('so-cart');
+    const item = e.target.dataset.id
+    for(let i = 0; i < cartContents.length; i++) {
+      console.log(cartContents[i]);
+      if(cartContents[i].Id == item) {
+          cartContents.splice(i, 1);
+          break;
+      }
+    }
+    setLocalStorage('so-cart', cartContents);
+    this.renderList(cartContents);
+  }
+
   prepareTemplate(template, product) {
     
     template.querySelector('.cart-card__image img').src =  product.Images.PrimaryMedium;
@@ -19,6 +34,7 @@ export default class CartList {
     template.querySelector('.card__name').textContent = product.Name;
     template.querySelector('.cart-card__color').textContent = product.Colors[0].ColorName;
     template.querySelector('.cart-card__price').textContent += product.FinalPrice; 
+    template.querySelector('.removeFromCart').setAttribute('data-id', product.Id); 
     return template;
   }
   
@@ -28,6 +44,7 @@ export default class CartList {
     //get the template
     const template = document.getElementById('cart-card-template');
     renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
-    
+    const items = getLocalStorage('so-cart');
+    document.getElementById("totalCartItems").innerHTML = ("Total Items: " + items.length);
   }
 }
