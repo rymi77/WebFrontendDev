5303ece1cf4353d449aa5863ba58f619c766746e
rymi77 <mil16035@byui.edu>
2021-02-22 16:45:13 -0700

    finished team activity

--- build/js/product.js
+++ build/js/product.js
@@ -1,10 +1,10 @@
 
-import ProductData from './productData.js';
+import ExternalServices from './ExternalServices.js';
 import {getParams} from "./utils.js";
 import ProductDetails from './productDetails.js';
 
 const productId = getParam('product');
-const dataSource = new ProductData('tents');
+const dataSource = new ExternalServices('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
