c89c6f0ed4409ced8ffc1dd0ed317ddf707eb6ea
rymi77 <mil16035@byui.edu>
2021-02-23 11:40:22 -0700

    checked form

--- src/js/externalServices.js
+++ src/js/externalServices.js
@@ -1,35 +1,37 @@
 const baseURL = 'http://157.201.228.93:2992/'
 
-function convertToJson(t) {
-    if (t.ok) {
-      return t.json();
-    } else{
-      throw new Error("Bad Response");
-    }
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
+  } else{
+    let jsonResponse = JSON.stringify(data)
+    throw { name: 'servicesError', message: jsonResponse };
+  }
 }
 
 class ExteralServices{
-    constructor() {
+  constructor() {
+  }
+  
+  getData(category) {
+    return fetch(baseURL + `products/search/${category}`)
+    .then(convertToJson).then((data) => data.Result);
     }
-    
-    getData(category) {
-      return fetch(baseURL + `products/search/${category}`)
-      .then(convertToJson).then((data) => data.Result);
-      }
 
-    async findProductById(id) {
-      return fetch(baseURL + `product/${id}`)
-      .then(convertToJson).then((data) => data.Result);
-    }
-    async checkout(payload) {
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      };
-      return await fetch(baseURL + 'checkout/', options).then(convertToJson);
-    }
+  async findProductById(id) {
+    return fetch(baseURL + `product/${id}`)
+    .then(convertToJson).then((data) => data.Result);
+  }
+  async checkout(payload) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
+  }
 }
 export default ExteralServices;
